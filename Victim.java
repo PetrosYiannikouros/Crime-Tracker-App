a83b976adc845298dddb592b6a78ce33a88c2abf
tylershatley <shatleymtyler@gmail.com>
2021-03-09 16:45:30 -0500

    added victim stub

---
+++ Victim.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+/**
+ * Victim class
+ * creates victim
+ * adds stories and family members
+ * extends class person
+ */
+public class Victim extends Person {
+    private List<String> stories;
+    private List<String> familyMembers;
+
+    /**
+     * default constructor
+     */
+    public Victim() {
+    }
+
+    /**
+     * going to have to come back too later. I dont think just a normal list
+     * is going in the parameters.
+     * @param list
+     * @return list of description of criminal
+     */
+    public List<String> descriptionOfCriminal(List <String> list) {
+        return null;
+    }
+
+}
