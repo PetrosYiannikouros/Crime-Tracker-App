757eceb4db2297ce281e623f96052d4ee5366b78
PetrosYiannikouros <yianniko@email.sc.edu>
2021-03-09 14:39:03 -0500

    added front-end stub

---
+++ FrontEnd.java
@@ -0,0 +1,17 @@
+public class FrontEnd {
+    public static void main(String[] args) {
+
+    }
+
+    public static void promptForUsernameAndPassword() {
+    }
+
+    public static void addCrime() {
+    }
+
+    public static void promtForPerson() {
+    }
+
+    private static void displayCrime() {
+    }
+}
