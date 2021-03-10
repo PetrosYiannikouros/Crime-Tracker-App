97783edcc188f1d0fb6595ef7aba8d83c7f29b52
tylershatley <shatleymtyler@gmail.com>
2021-03-10 15:28:21 -0500

    added officer stub

---
+++ Officer.java
@@ -0,0 +1,16 @@
+/**
+ * class for officers
+ * typer of user
+ */
+public class Officer extends User {
+    private int yearsServed;
+    private String rank;
+    private String badgeNumber;
+
+    /**
+     * Default constructor for Officer
+     */
+    public Officer() {
+
+    }
+}
