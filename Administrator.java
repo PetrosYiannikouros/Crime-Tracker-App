ab58fdfbc3bb46785f74296aa30954c5894c17d5
tylershatley <shatleymtyler@gmail.com>
2021-03-10 15:39:59 -0500

    added Admin stub

---
+++ Administrator.java
@@ -0,0 +1,36 @@
+/**
+ *  class administrator
+ * extentsion of type of user
+ */
+public class Administrator extends User{
+    private String phoneNumber;
+    private String  email;
+
+    /**
+     * Default Constructor for admin
+     */
+    public Administrator() {
+
+    }
+
+    /**
+     * void method for deleting a crime
+     */
+    private void deleteCrime() {
+
+    }
+
+    /**
+     * void method for deleting a person
+     */
+    private void deletePerson() {
+
+    }
+
+    /**
+     * void method for deleting users
+     */
+    private void deleteUser() {
+
+    }
+}
