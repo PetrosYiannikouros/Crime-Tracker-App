27791a2773c4a5b05c620d80ad73579bcb238cb6
tylershatley <shatleymtyler@gmail.com>
2021-03-10 16:13:09 -0500

    updated Officer stub

--- Officer.java
+++ Officer.java
@@ -13,4 +13,51 @@ public class Officer extends User {
     public Officer() {
 
     }
+
+    //getter section
+    /**
+     * getter for years served by an Officer
+     * @return int yearsServed
+     */
+    public int getYearsServed() {
+        return yearsServed;
+    }
+    /**
+     * getter for rank of an Officer 
+     * @return String rank
+     */
+    public String getRank() {
+        return rank;
+    }
+    /**
+     * getter for Officer badge Number
+     * gonig to be used for identifcation
+     * @return String badgeNumber
+     */
+    public String getBadgeNumber() {
+        return badgeNumber;
+    }
+
+    //setter section 
+    /**
+     * set amount of years an Officer has served
+     * @param int yearsServed
+     */
+    public void setYearsServed(int yearsServed) {
+        this.yearsServed = yearsServed;
+    }
+    /**
+     * set the rank of an officer
+     * @param String rank
+     */
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    /**
+     * set the badge number for an officer
+     * @param String badgeNumber
+     */
+    public void setBadgeNumber(String badgeNumber) {
+        this.badgeNumber = badgeNumber;
+    }
 }
