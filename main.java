d080509af84762a9d9a396cdbb0686f987dbe544
tylershatley <shatleymtyler@gmail.com>
2021-03-16 13:56:26 -0400

    main

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
         Officers test = Officers.getInstance();
-        System.out.println(test.getOfficers());
+        test.getOfficers();
+        //System.out.println(test.getOfficers());
     }
 }
