a5e15e97f1646ed601821378ee8925101bd4d429
Reece Peters <ppeters@email.sc.edu>
2021-03-16 06:12:37 -0500

    Implemented Victim Class

--- Victim.java
+++ Victim.java
@@ -10,6 +10,7 @@ public class Victim extends Person {
     private String story;
     private List<String> familyMembers;
     private String phoneNumber;
+    private String criminalDesc;
 
     /**
      * default constructor
@@ -20,22 +21,90 @@ public class Victim extends Person {
     }
     public Victim (String firstName, String lastName, 
                 boolean deceased, List<Integer> caseNums, String story,
-                List<String> familyMembers,  String phoneNumber)
+                List<String> familyMembers,  String phoneNumber, String criminalDesc)
     {
         super(firstName, lastName, deceased, caseNums);
         this.story = story;
         this.familyMembers = familyMembers;
         this.phoneNumber = phoneNumber;
+        this.criminalDesc = criminalDesc;
+    }
+
+    //getters -----------------------------------------------------
+
+    /**
+     * 
+     * @return
+     */
+    public String getStory() {
+        return this.story;
     }
 
     /**
-     * going to have to come back too later. I dont think just a normal list
-     * is going in the parameters.
-     * @param list
-     * @return list of description of criminal
+     * 
+     * @return
      */
-    public List<String> descriptionOfCriminal(List<String> list) {
-        return;
+    public List<String> getFamily() {
+        return this.familyMembers;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getCriminalDesc() {
+        return this.criminalDesc;
+    }
+
+    //setters -----------------------------------------------------
+
+    /**
+     * 
+     * @param story
+     */
+    public void setStory(String story) {
+        this.story = story;
+    }
+
+    /**
+     * 
+     * @param familyMembers
+     */
+    public void setFamilyMembers(List<String> familyMembers) {
+        this.familyMembers = familyMembers;
+    }
+
+    /**
+     * 
+     * @param number
+     */
+    public void setPhoneNumber(String number) {
+        this.phoneNumber = number;
+    }
+
+    /**
+     * 
+     * @param description
+     */
+    public void setCriminalDesc(String description) {
+        this.criminalDesc = description;
+    }
+
+
+    /**
+     * 
+     * @param familyMember
+     */
+    public void addFamilyMember(String familyMember) {
+        familyMembers.add(familyMember);
     }
 
 }
