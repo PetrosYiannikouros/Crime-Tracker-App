e77c627d2d1f1ad8cbe35e8596d574c14c67bbd4
tylershatley <shatleymtyler@gmail.com>
2021-03-22 10:36:48 -0700

    addd victim stuff

--- Victims.java
+++ Victims.java
@@ -1,3 +1,32 @@
+/**
+ * Victims class
+ * used for data processing
+ */
+import java.util.ArrayList;
+import java.util.List;
 public class Victims {
+    private static Victims victims = null;
+    private static ArrayList<Victim> victimList = new ArrayList<Victim>();
 
+    private Victims() {
+        victimList = DataLoader.loadVictims();
+    }
+
+    public static Victims getInstance() {
+        if (victims == null) {
+            victims = new Victims();
+        }
+        return victims;
+    }
+
+    public ArrayList<Victim> getVictims() {
+        return victimList;
+    }
+
+    public void addVictim(String firstName, String lastName, String phoneNumber,
+        List<String> familyMembers, String criminalDesc) {
+        victimList.add(new Victim(firstName, lastName, phoneNumber, 
+        familyMembers, criminalDesc));
+        DataWriter.saveVictims();
+    }
 }
