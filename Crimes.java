87996f89dac8087ff77cc31ad5658e5f2a866a09
tylershatley <shatleymtyler@gmail.com>
2021-03-22 16:31:58 -0700

    crimes stuff

--- Crimes.java
+++ Crimes.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 public class Crimes {
+    private static Crimes crimes = null;
+    private static ArrayList<Crime> crimeList = new ArrayList<>();
 
+    private Crimes() {
+        crimeList = DataLoader.loadCrimes();
+    }
+
+    public static Crimes getInstance() {
+        if (crimes == null) {
+            crimes = new Crimes();
+        }
+        return crimes;
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return crimeList;
+    }
+
+    public void addCrime(int caseNumber, TypesOfCrimes type, String description,
+    SimpleDateFormat date, String address,
+    Officer assigned) {
+        crimeList.add(new Crime(caseNumber, type, description, date, address, assigned));
+        DataWriter.saveCrimes();
+    }
 }
