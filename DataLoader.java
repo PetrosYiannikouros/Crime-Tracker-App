a99c7c36deb5c7670d9bdd62280e29e37751e7d5
tylershatley <shatleymtyler@gmail.com>
2021-03-22 13:33:13 -0700

    added user stuff

--- DataLoader.java
+++ DataLoader.java
@@ -216,4 +216,41 @@ public class DataLoader extends DataConstants {
         }
         return null;
     }
+
+
+    public static ArrayList<User> loadUsers() {
+        ArrayList<User> users = new ArrayList<User>();
+
+        try {
+            FileReader reader = new FileReader(USER_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray usersJSON = (JSONArray) new JSONParser().parse(reader);
+
+            for (int i = 0; i < usersJSON.size(); ++i) {
+                JSONObject userJSON = (JSONObject) usersJSON.get(i);
+                String id = (String) userJSON.get(USER_ID);
+                String firstName = (String) officerJSON.get(OFFICER_FIRST_NAME);
+                String lastName = (String) officerJSON.get(OFFICER_LAST_NAME);
+                String userName = (String) officerJSON.get(OFFICER_USERNAME);
+                String password = (String) officerJSON.get(OFFICER_PASSWORD);
+                String precinct = (String) officerJSON.get(OFFICER_PRECINCT);
+                String department = (String) officerJSON.get(OFFICER_DEPARTMENT);
+                int yearsServed = ((Long) officerJSON.get(OFFICER_YEARS_SERVED)).intValue();
+                String rank = (String) officerJSON.get(OFFICER_RANK);
+                String badgeNumber = (String) officerJSON.get(OFFICER_BADGE_NUMBER);
+                officers.add(new Officer(firstName, lastName, userName, password, precinct, department, yearsServed,
+                        rank, badgeNumber));
+                System.out.println(OFFICER_ID + ": " + id + "\n" + OFFICER_FIRST_NAME + ": " + firstName + "\n"
+                        + OFFICER_LAST_NAME + ": " + lastName + "\n" + OFFICER_USERNAME + ": " + userName + "\n"
+                        + OFFICER_PASSWORD + ": " + password + "\n" + OFFICER_PRECINCT + ": " + precinct + "\n"
+                        + OFFICER_DEPARTMENT + ": " + department + "\n" + OFFICER_YEARS_SERVED + ": " + yearsServed
+                        + "\n" + OFFICER_RANK + ": " + rank + "\n" + OFFICER_BADGE_NUMBER + ": " + badgeNumber + "\n"
+                        + "***************************");
+            }
+            return officers;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
