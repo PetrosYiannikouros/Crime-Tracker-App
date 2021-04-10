44c485fc289881f6ddd6e6e0e7dac50b77263337
Rahulsubramanian46 <you@example.com>
2021-04-10 15:22:14 -0400

    s

--- tests/CrimeTest.java
+++ tests/CrimeTest.java
@@ -1,5 +1,14 @@
 package tests;
+import java.util.ArrayList;
 
 public class CrimeTest {
-    
+    private Crime crime = Crime.getInstance();
+    private ArrayList<Crime> crimeList = crime.getCrime();
+
+    public void setup() {
+        crimeList.clear();
+        crimeList.add(new Crime());
+        crimeList.add(new Crime());
+        DataWriter.saveCrimes();
+    }
 }
