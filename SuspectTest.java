4c638cf26c5680bb2a8055af58f9d0bb527ee45e
PetrosYiannikouros <yianniko@email.sc.edu>
2021-04-11 21:41:34 -0400

    updated tests

--- SuspectTest.java
+++ SuspectTest.java
@@ -8,41 +8,30 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class SuspectTest {
     private Suspects suspects = Suspects.getInstance();
-    private ArrayList<Suspect> suspectList = suspects.getEvidences();
+    private ArrayList<Suspect> suspectList = suspects.getSuspects();
 
     @BeforeEach
-	public void setup() {
-		suspectList.clear();
-        List<String> reasons = new ArrayList<>();
-        reasons.add("Same make and model of car reported at crime");
-        suspectList.add(new Suspect("Louis", "Emmery", "4347774234", "55 Reach Rd.", "Emerald",
-        24, 178, "6'3", "White", "Male", "brown", 10, "Black", "long", "none", 
-        "Black hoodie and red shoes", false, true, "Sedan Toyota", "33r-ty6", reasons));
-		DataWriter.saveSuspects();
-	}
-	
-	@AfterEach
-	public void tearDown() {
-		Suspects.getInstance().getSuspects().clear();
-		DataWriter.saveSuspects();
-	}
-	
-	//Reason adding test
+    public void setup() {
+        suspectList.clear();
+        suspectList.add(new Suspect("Louis", "Emmery", "4347774234", "55 Reach Rd.", "Emerald", 24, 178, "6'3", "White",
+                "Male", "brown", 10, "Black", "long", "none", "Black hoodie and red shoes", false, true, "Sedan Toyota",
+                "33r-ty6"));
+        DataWriter.saveSuspects();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Suspects.getInstance().getSuspects().clear();
+        DataWriter.saveSuspects();
+    }
+
+    // Reason adding test
     @Test
     void testAddReason() {
         String reason = "Owns car similar to reported vehicle in crime";
-        boolean isReasonAdded = Suspect.addReason(reason);
-        assertTrue(isReasonAdded);
+        suspectList.get(0).addReason(reason);
+        assertEquals("Owns car similar to reported vehicle in crime", suspectList.get(0).getReasons().get(0));
     }
- 
-    @Test
-    void testNoReasonAdded() {
-        String reason = "";
-        boolean isReasonAdded = Suspect.addReason(reason);
-        assertFalse(isReasonAdded);
-    }
-
 }
--- VictimTest.java
+++ VictimTest.java
@@ -10,12 +10,11 @@ import org.junit.jupiter.api.Test;
 
 import jdk.jfr.Timestamp;
 
-
 public class VictimTest {
 
-    //test constructors since no functions other than getters and setters
+    // test constructors since no functions other than getters and setters
     @Test
-	public void constructorTest() {
+    public void constructorTest() {
         String firstName = "Karl";
         String lastName = "Baker";
         String phoneNumber = "9447387622";
@@ -23,17 +22,14 @@ public class VictimTest {
         String story = "Laying outside, bag was taken";
         String criminalDesc = "black male, late 30's wearing black mask";
 
+        Victim victim = new Victim(firstName, lastName, phoneNumber, address, story, criminalDesc);
 
-        Victim victim = new Victim(firstName, lastName, phoneNumber, address, 
-        story, criminalDesc);
-        
-        assertEquals(firstName, victim.getFirstName);
-        assertEquals(lastName, victim.getLastName);
-        assertEquals(phoneNumber, victim.getPhoneNumber);
-        assertEquals(address, victim.getAddress);
-        assertEquals(story, victim.getStory);
-        assertEquals(criminalDesc, victim.getCriminalDesc);
-	}
+        assertEquals(firstName, victim.getFirstName());
+        assertEquals(lastName, victim.getLastName());
+        assertEquals(phoneNumber, victim.getPhoneNumber());
+        assertEquals(address, victim.getAddress());
+        assertEquals(story, victim.getStory());
+        assertEquals(criminalDesc, victim.getCriminalDesc());
+    }
 
 }
-
