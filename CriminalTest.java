bf399278b6e7b747216dc12f40ea140797854672
PetrosYiannikouros <yianniko@email.sc.edu>
2021-04-11 21:54:23 -0400

    updated Criminal test

--- CriminalTest.java
+++ CriminalTest.java
@@ -8,43 +8,31 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class CriminalTest {
-    private Criminals criminals = Criminals.getInstance();
-    private ArrayList<Criminal> criminalList = criminals.getCriminals();
+	private Criminals criminals = Criminals.getInstance();
+	private ArrayList<Criminal> criminalList = criminals.getCriminals();
 
-    @BeforeEach
+	@BeforeEach
 	public void setup() {
 		criminalList.clear();
 		// reset, check, add necessary testing parameters
-                List<String> convictions = new ArrayList<>(); 
-                convictions.add("Harassment");
-                convictions.add("Misdimeanor");
-        	criminalList.add(new Criminal("Paul", "Humfrey", "7045555555", "42 Sydney St.", "two toes", 32, 146, "5'4", "white", "male", "blue",
-                16, "brown", "short", "scruff", "black pants", true, true, "Honda SUV", "l34-a2d", true, convictions));
-        	DataWriter.saveCriminals();
+		criminalList.add(new Criminal("Paul", "Humfrey", "7045555555", "42 Sydney St.", "two toes", 32, 146, "5'4",
+				"white", "male", "blue", 16, "brown", "short", "scruff", "black pants", true, true, "Honda SUV",
+				"l34-a2d", true));
+		DataWriter.saveCriminals();
 	}
-	
+
 	@AfterEach
 	public void tearDown() {
 		Criminals.getInstance().getCriminals().clear();
 		DataWriter.saveCriminals();
 	}
-	
+
 	// test for adding convictions
 	@Test
 	void testAddConviction() {
-        	String conviction = "";
-		boolean isConvictionAdded = Criminal.addConviction(conviction);
-		assertTrue(isConvictionAdded);
+		String conviction = "2013 Grand Theft Auto";
+		criminalList.get(0).addConvictions(conviction);
+		assertEquals("2013 Grand Theft Auto", criminalList.get(0).getConvictions().get(0));
 	}
-
-    	@Test
-	void testNoConvictionAdded() {
-        	String conviction = "";
-		boolean isConvictionAdded = Criminal.addConviction(conviction);
-		assertFalse(isConvictionAdded);
-	}
-	
 }
-
